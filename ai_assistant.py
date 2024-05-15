c5120cab26283c6f917fbe0b867522b8956ed128
gg <129578538+mynoveldownloads@users.noreply.github.com>
2024-05-15 13:01:02 +0800

    Update ai_assistant.py

--- ai_assistant.py
+++ ai_assistant.py
@@ -1,6 +1,6 @@
 from functions import prompt
 from stats import *
-from instructions_list import list_of_instructions
+from instructions_list import list_of_instructions as inst
 
 xyz = [1+i for i in range(50, 101)]
 hh = [10+i for i in range(50, 101)]
@@ -10,7 +10,7 @@ pp = [10*i for i in range(50, 101)]
 def menu():
     while True:
         user_prompt = input('>>> ')
-        bot = prompt(user_prompt, list_of_instructions(user_prompt))
+        bot = prompt(user_prompt, inst(user_prompt))
         #print(bot) -> hint for function calling
         execute = f'print(\'Answer:\', {bot})'
         exec(execute)
@@ -18,3 +18,14 @@ def menu():
 
 if __name__ == '__main__':
     menu()
+
+"""
+Sample prompts:
+- give median xyz
+- give median xyz, hh, pp
+- what is variance of xyz, hh
+- variance of xyz, hh, pp
+- give mean of xyz, hh, pp
+- sd of xyz, pp
+- give sd of xyz, hh and pp
+"""
